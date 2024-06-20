ec64c8a5b2192b9b2af25ced0557bc6480d0d5df
kaiser.reid.ronald.1265277787 <reid.kaiser.ctr@us.af.mil>
2024-06-20 09:43:31 -0600

    Adds more options to include date ranges

--- README.md
+++ README.md
@@ -1,33 +1,40 @@
 # Gitlab Issue Retriever
-Using the gitlab API this tool will read the issues that exist in the gitlab project and output them as a markdown list into a file that can be used to produce the release notes. This tool expects an argument that identifies the repository from which the issues will be querried. By default this tool will look for those issues that have been labeled `Release`. It is also configured to read the environment for the id of the resource as well as the token to the repository.
+Using the gitlab API this tool will read the issues that exist in the gitlab project and output them as a markdown list into a file that can be used to produce release notes. This tool expects an argument that identifies the repository from which the issues will be querried. By default this tool will look for those issues that have been labeled `Release`. It is also configured to read the environment for the id of the resource as well as the token to the repository.
 
 You can use node to execute the command. The following are the options that are available with command:
 ```
+
 Arguments:
-    string                   URL to the gitlab repository including protocol
+  string                   URL to the gitlab repository including protocol
 
 Options:
-    -l, --label <string>     gitlab label to search (default: "Release")
-    -r, --resource <string>  gitlab resource to search, projects or groups (default: "projects")
-    -f, --file <string>      output file (default: "file.md")
-    -i, --id <number>        id of project or group to search (env: GITLAB_RESOURCE_ID)
-    -t, --token <string>     token for repo (env: REPO_TOKEN)
-    -h, --help               display help for command
+  -l, --label <string>     gitlab label to search, i.e. Release
+  -r, --resource <string>  gitlab resource to search, projects or groups (default: "projects")
+  -s, --state <string>     issue state, can be all, opened, or closed (default: "all")
+  -f, --file <string>      output file (default: "file.md")
+  -a, --after <string>     closed after date
+  -b, --before <string>    closed before date
+  -i, --id <number>        id of project or group to search (env: GITLAB_RESOURCE_ID)
+  -t, --token <string>     token for repo (env: REPO_TOKEN)
+  -h, --help               display help for command
 ```
 
-
-If you are using the token and project id from the environment you can retrieve all issues that are tagged `Release` using the following command:
+If you are using the token and project id from the environment you can retrieve all issues that are tagged `Release` and not closed using the following command:
 ```bash
-npx ts-node index.ts https://myrepo.site.com
+npx ts-node index.ts https://myrepo.site.com --label Release --state opened
 ```
-
 
 To retrieve all issues that are tagged `Vetted` while still using the environment for id and token, use the following command:
 ```bash
-npx tx-node index.ts --label Vetted https://myrepo.site.com
+npx ts-node index.ts --label Vetted https://myrepo.site.com
 ```
 
 To specify a different resource (i.e. group instead of project) and want to specify the id, use the following command:
 ```bash
 npx ts-node index.ts --resource groups --id 1234 https://myrepo.site.com
+```
+
+To specify a closed date range you can use the following command:
+```bash
+npx ts-node index.ts https://myrepo.site.com --after 2024/01/01 --before 2024/06/01
 ```
--- index.ts
+++ index.ts
@@ -1,47 +1,65 @@
 import axios, { AxiosResponse } from 'axios';
-import { Command, Option } from 'commander';
+import { Command, Option, OptionValues } from 'commander';
 import fs from 'fs';
 import path from 'path';
 
 const program = new Command();
 program
     .argument('<string>', 'URL to the gitlab repository including protocol')
-    .option('-l, --label <string>', 'gitlab label to search', 'Release')
+    .option('-l, --label <string>', 'gitlab label to search, i.e. Release')
     .option('-r, --resource <string>', 'gitlab resource to search, projects or groups', 'projects')
-    .option('-f, --file <string>', 'output file', 'file.md');
+    .option('-s, --state <string>', 'issue state, can be all, opened, or closed', 'all')
+    .option('-f, --file <string>', 'output file', 'file.md')
+    .option('-a, --after <string>', 'closed after date')
+    .option('-b, --before <string>', 'closed before date');
 program.addOption(new Option('-i, --id <number>', 'id of project or group to search').env('GITLAB_RESOURCE_ID')); // 15124
 program.addOption(new Option('-t, --token <string>', 'token for repo').env('REPO_TOKEN'));
 
 program.parse(process.argv);
 const options = program.opts();
-retrieveIssues(program.args[0], options.resource, options.id, options.label, options.token, options.file);
+retrieveIssues(program.args[0], options.resource, options.id, options.label, options.token, options.file, options.state, options);
 
 /**
  * Retrieves the issues and saves them in the file.
  * @param repoUrl is the url including the protocol to the repository
  * @param resource is the type of resource that is being querried (groups | projects)
  * @param id is the id for the resource that is being querried.
- * @param label is the labelled that is being requested
+ * @param label is the label that is being requested
  * @param token is the access token that is used to access the gitlab resource
  * @param file is the output file
+ * @param state is the state (i.e. all, opened, or closed)
+ * @param options are all the options provided to the program
  */
-async function retrieveIssues(repoUrl: string, resource: 'groups' | 'projects', id: number, label: string, token: string, file: string) {
+async function retrieveIssues(repoUrl: string, resource: 'groups' | 'projects', id: number, label: string, token: string, file: string, state: string, options: OptionValues) {
     const apiUrl = `${repoUrl}/api/v4/${resource}/${id}/issues`;
     let page = 1;
     const issues: any[] = [];
-    const requestResponse = await executeRequest('HEAD', page, token, apiUrl, label);
+    const requestResponse = await executeRequest('HEAD', page, token, apiUrl, label, state);
     const totalPages = requestResponse.headers['x-total-pages'];
+    // Execute requests to gitlab server
     while (totalPages > 0 && page <= totalPages)  {
-        const responseWithIssues = await executeRequest('GET', page, token, apiUrl, label);
+        const responseWithIssues = await executeRequest('GET', page, token, apiUrl, label, state);
         page++;
         responseWithIssues.issues.forEach((issue) => issues.push(issue));
     }
     const fileWriteStream = fs.createWriteStream(path.join(__dirname, file));
-    issues.forEach((issue: any) => {
+    let filteredIssues: any = issues;
+  // Date Range Filter
+  if (options.after || options.before) {
+    filteredIssues = issues.filter((issue) => issue.closed_at)
+      .filter((issue) => options.after ? (new Date(Date.parse(issue.closed_at))).getTime() > (new Date(Date.parse(options.after))).getTime() : true)
+      .filter((issue) => options.before ? (new Date(Date.parse(issue.closed_at))).getTime() < (new Date(Date.parse(options.before))).getTime() : true);
+
+    console.log('using date range have this many closed, %o', filteredIssues.length);
+    console.log('translated date range is %o - %o', (new Date(Date.parse(options.after))).toISOString(), (new Date(Date.parse(options.before))).toISOString());
+  }
+
+    filteredIssues.forEach((issue: any) => {
         // Format should look similar to the following in order to paste in to markdown format:
         //  - [IBFE-840](https://path/to/issue/with/id/number) - Issue Title
         fileWriteStream.write(`- [IBFE-${issue.iid}](${issue.web_url}) - ${issue.title}\n`);
     });
+
     fileWriteStream.end();
     fileWriteStream.on('close', () => {
         console.log('finished writing file');
@@ -61,12 +79,21 @@ async function retrieveIssues(repoUrl: string, resource: 'groups' | 'projects', 
  * @param label is the label that is associated with the issue being returned
  * @returns 
  */
-async function executeRequest(method: string, page: number, token: string, apiUrl: string, label: string): Promise<{headers: any; issues: any[]}> {
+async function executeRequest(method: string, page: number, token: string, apiUrl: string, label: string, state: string = 'opened'): Promise<{headers: any; issues: any[]}> {
     axios.create();
+    const params = {scope: 'all', state: state, per_page: 100, page: page};
+    if (label) {
+      Object.assign(params, {labels: label});
+    }
+    console.log('params are %o', params);
+    console.log('url %o', apiUrl);
     const config = {
         method: method,
         url: apiUrl,
-        params: {labels: label, scope: 'all', state: 'opened', per_page: 100, page: page},
+        // scope can be `all`, `assigned_to_me`, or `created_by_me`.
+        // state can be `all` issues or limited to those that are `opened` or `closed`.
+        // params: {labels: label, scope: 'all', state: state, per_page: 100, page: page},
+        params: params,
         headers: {
             'PRIVATE-TOKEN': token
         }
